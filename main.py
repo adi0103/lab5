1a0de66994847fbe0c93a36aaf8c5bf7638ee7d4
ulkshifer <kamza.0508@gmail.com>
2022-10-19 13:28:08 +0600

    6 commit

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-d = {}
-n = int(input('Студенттердің санын енгізіңіз :  '))
-# создаем пустой лист
-# информациия о студенте
-list = []
-for i in range(0, n):
-	# split
-	x,y = input("Студенттің аты - жөні және бағасы : ").split()
-	#  x - имя , y - балл
-	list.append((y,x))
-  #сортировка по баллу
-list = sorted(list, reverse = False)
-print('Өсу реті бойынша өңделген тізім : ')
-for i in list:
-	print(i[1], i[0])
+
+# task
+a = [["Adilet", 100],
+     ["Aidos", 95]]
+name = input("name:")
+for i in range(len(a)):
+    for j in range(len(a)):
+        if name == a[i][0]:
+            print(a[i][0], ":", a[i][1])
+            break
+        else:
+            print("Student jok!")
+            break
