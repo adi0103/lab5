da40f113c6e00fbd84c6536041751f279cd6ab1f
ulkshifer <kamza.0508@gmail.com>
2022-10-19 09:15:05 +0600

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-a=list(map(int, input().split()))
-b = []
-for i in range(1,len(a)-1):
-    if a[i]>a[i-1] and a[i]>a[i+1]:
-        b.append(a[i])
-for i in range (0, len(b)):
-      print(b)
+a=input().split()
+p=a[-1]
+for i in range(len(a)-2, -1, -1):
+    a[i+1]=a[i]
+a[0]=p
+print(' '.join(a))
